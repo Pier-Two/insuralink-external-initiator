68f62a6f8bacf2ad61f49592e8c113390b1d6dd3
raymogg <raymondmogg@gmail.com>
2020-02-15 11:05:55 -0700

    Init commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.sendStatus(200);
+})
+
+app.get("/temp", function(req, res) {
+    res.send({'temperature': 42})
+});
+
+app.post("/temp", function(req, res) {
+    console.log(req.body)
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
