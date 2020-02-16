75a81274c5d443a33afed69125d80430cc7a3321
raymogg <raymondmogg@gmail.com>
2020-02-16 03:46:02 -0700

    Updated service

--- server.js
+++ server.js
@@ -11,54 +11,62 @@ const SENSOR_IP = "http://10.0.1.13"
 const CHAINLINK_IP = "http://35.226.187.4:6688"
 const CHAINLINK_ACCESS_KEY = "934de8d11ba04776983c88c08dcc4391"
 const CHAINLINK_ACCESS_SECRET = "0U8n49W8a3nFUChxZvKNDXrCAY2Ltec4cG2kNmMY0AUMxUdvlPGOr1c2W/W3mAnD"
-const INSURALINK_ADDRESS = "0x08e8de6956b869b2C656c4742f6Af12fCeB91e9d"
-//var requestID = "27088baff0bf4e0b8653e2ac3b1c77c4"
-var requestID = "0202447b75984769b517927f43da4bb5"
-var lastTemp = 0
-var lastFireTime = 0
+const INSURALINK_ADDRESS = "0xfcB3C8Cfe463F441A14d2c511A67dA02344985e4"
+var JOB_ID = "0202447b75984769b517927f43da4bb5"
+var TEST_DAI = "0xBc980E67F6122F6E55fBeb9893A70c848d288B25"
+//var KYBER_TEST_DAI = "0xad6d458402f60fd3bd25163575031acdce07538d"
+
+var lastFireTime = Math.round((new Date()).getTime() / 1000)
 var THRESHOLD = 30
 var currentContractId = 0
 
 //Setup web3
-//On server startup pull the latest currentContractId from the contract to stay in sync
 var Web3 = require('web3');
 var web3 = new Web3('wss://ropsten-rpc.linkpool.io/ws');
-// var insuralinkContract = new web3.eth.Contract(insuralink.abi, INSURALINK_ADDRESS)
-// insuralinkContract.methods.activeCounter().call().then((result) => {
-//     console.log(result)
-//     currentContractId = result
-// })
+//On server startup pull the latest currentContractId from the contract to stay in sync
+updateCurrentActiveJob()
 
 app.get('/', function (req, res) {
    res.sendStatus(200);
 })
 
+/** Called by chainlink node when a job is created */
 app.post('/', function (req, res) {
     //Recieves info from node about the job id
+    //TODO store multiple job id's
     console.log(req.body)
-    requestID = req.body.jobId
+    JOB_ID = req.body.jobId
     res.sendStatus(200);
  })
 
+/** Called by chainlink node when running the job */ 
 app.get("/temp", function(req, res) {
     res.send({'id': currentContractId})
     currentContractId++;
     console.log(currentContractId)
 });
 
-app.post("/temp", function(req, res) {
-    console.log(req)
-    console.log(req.body)
-    res.sendStatus(200)
-});
+// app.post("/temp", function(req, res) {
+//     console.log(req)
+//     console.log(req.body)
+//     res.sendStatus(200)
+// });
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
-  });
+});
+
+async function updateCurrentActiveJob() {
+    var insuralinkContract = new web3.eth.Contract(insuralink.abi, INSURALINK_ADDRESS)
+    insuralinkContract.methods.activeCounter().call().then((result) => {
+    console.log(result)
+    currentContractId = result
+    })
+}
 
 function callChainlinkNode() {
-    var url_addon = '/v2/specs/'+ requestID + '/runs'
+    var url_addon = '/v2/specs/'+ JOB_ID + '/runs'
     console.log(CHAINLINK_IP+url_addon)
     request.post({
         headers: {'content-type' : 'application/json', 'X-Chainlink-EA-AccessKey': CHAINLINK_ACCESS_KEY,
@@ -66,13 +74,12 @@ function callChainlinkNode() {
         url:     CHAINLINK_IP+url_addon,
         body:    ""
       }, function(error, response, body){
-        //console.log(error)
-        //console.log(response)
-        console.log(body);
+        updateCurrentActiveJob()
       });
     console.log("Job Sent")
 }
 
+//Poll sensors
 setInterval(function () {
     request(SENSOR_IP, function (error, response, body) {
         // console.log(error)
@@ -80,13 +87,15 @@ setInterval(function () {
         var data = JSON.parse(body)
         console.log(data)
         console.log(data.temperature)
-        console.log(data.temperature > THRESHOLD)
+        console.log(data.temperature < THRESHOLD)
         //TODO Sensor response logic
-        if (data.temperature > THRESHOLD && lastFireTime < Math.round((new Date()).getTime() / 1000) - 60) {
+        console.log(lastFireTime)
+        console.log(Math.round((new Date()).getTime() / 1000) - 60)
+        if (data.temperature < THRESHOLD && lastFireTime < Math.round((new Date()).getTime() / 1000) - 15) {
             lastFireTime = Math.round((new Date()).getTime() / 1000);
             //Call chainlink node
             console.log("FIRING JOB")
-            console.log(body.temperature)
+            console.log(data.temperature)
             callChainlinkNode()
         }
     });
